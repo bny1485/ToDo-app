4b7cc41c9e8b26206fdf3e28a44e102db6354458
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-09 15:28:53 +0330

    create user module

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 
 from flask import Flask
 from main.routes import main
+from user.routes import users
 
 
 app = Flask(__name__)
 app.register_blueprint(main)
+app.register_blueprint(users)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
