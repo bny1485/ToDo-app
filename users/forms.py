945a3038daa93fa706851c833f51fd1a89acd956
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-11 19:48:55 +0330

    Registration Form

---
+++ users/forms.py
@@ -0,0 +1,12 @@
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class RegistrationForm(FlaskForm):
+    user_name = StringField('username', validators=[DataRequired(), Length(min=5, max=32, message='Your username must be at least 5 words and at most 32 words')])
+    email = StringField('Email addrese', validators=[DataRequired(), Email()])
+    password = PasswordField('Password')
+    confirm_password = PasswordField('Confirm Password')
+    submit = SubmitField('Sign Up')
--- users/routes.py
+++ users/routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from users.forms import RegistrationForm
 
 users = Blueprint('users', __name__)
 
 @users.route('/register')
 def register():
-    return render_template('register.html', title='register')
+    form = RegistrationForm()
+    return render_template('register.html', title='register', form=form)
