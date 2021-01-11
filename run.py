75ef23db2d392eb92dac84c321e8b389c5d20134
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-11 19:46:02 +0330

    generate secrat key

--- run.py
+++ run.py
@@ -2,12 +2,15 @@
 from flask import Flask
 from main.routes import main
 from users.routes import users
+from utils import random_string
+
 
 
 app = Flask(__name__)
 app.register_blueprint(main)
 app.register_blueprint(users)
 
+app.config['SECRET_KEY'] =  random_string
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,13 @@
+
+from random import randint
+
+def secret_key_generator():
+    w = 'abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ~!@#$%^&*<>|()?>0123456789'
+    random_string = ''
+    for _i in range(50):
+        chose = randint(0, len(w)-1)
+        random_string += w[chose]
+    return random_string
+
+
+random_string = secret_key_generator()
