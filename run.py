17ecc13fe5319f65ab182bfa5e812aad503d0e5e
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-14 00:35:21 +0330

    set up database use sqlalchemy

--- run.py
+++ run.py
@@ -4,13 +4,18 @@ from main.routes import main
 from users.routes import users
 from utils import random_string
 
-
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SECRET_KEY'] =  random_string
+
+
 app.register_blueprint(main)
 app.register_blueprint(users)
 
-app.config['SECRET_KEY'] =  random_string
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task.db'
+db = SQLAlchemy(app)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
