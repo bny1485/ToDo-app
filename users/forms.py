e13b5fcf833142bf66345a79b85187aa7b46f05f
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-17 20:59:23 +0330

    upgrade users form

--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,26 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from models import User
 
 
 class RegistrationForm(FlaskForm):
-    user_name = StringField('username', validators=[DataRequired(), Length(min=5, max=32, message='Your username must be at least 5 words and at most 32 words')])
+    user_name = StringField('username', validators=[DataRequired(), Length(min=2, max=32)])
     email = StringField('Email addrese', validators=[DataRequired(), Email()])
-    password = PasswordField('Password')
-    confirm_password = PasswordField('Confirm Password')
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm-Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, user_name):
+        user = User.query.filter_by(user_name=user_name.data).first()
+        if user:
+            raise ValidationError('User is alredy exist')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('email is alredy exist')
 
 class LoginForm(FlaskForm):
     email = StringField('Email addrese', validators=[DataRequired(), Email()])
