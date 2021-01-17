bf47dcb6c9ff0e9879e0a01a648620448bfcc43e
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-17 21:08:43 +0330

    register

--- run.py
+++ run.py
@@ -1,20 +1,22 @@
 
 from flask import Flask
-from main.routes import main
-from users.routes import users
 from utils import random_string
-
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] =  random_string
-
-
-app.register_blueprint(main)
-app.register_blueprint(users)
-
-
+app.config['SECRET_KEY'] = random_string
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+
+
+from users.routes import users
+from main.routes import main
+app.register_blueprint(main)
+app.register_blueprint(users)
 
 
 if __name__ == "__main__":
--- users/routes.py
+++ users/routes.py
@@ -1,17 +1,25 @@
-from flask import Blueprint, render_template
+""" this is route file """
+from flask import Blueprint, render_template, redirect, url_for
 from users.forms import RegistrationForm, LoginForm
+from run import db, bcrypt
+from models import User
 
 
 users = Blueprint('users', __name__)
 
 
-@users.route('/register')
+@users.route('/register', methods=['GET', 'POST'])
 def register():
+    ''' this function register user and if user was login user can not come to this page '''
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
     form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        new_user = User(user_name=form.user_name.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('users.login'))
     return render_template('register.html', title='register', form=form)
 
 
-@users.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='login', form=form)
