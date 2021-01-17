6998b20b39c5e3295ba9cd97cd088f54847e9b0a
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-17 21:01:43 +0330

    upgrade home route

--- main/routes.py
+++ main/routes.py
@@ -3,6 +3,8 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
-def hello_world():
-    return render_template('main.html',title='home')
+@main.route('/home')
+def home():
+    return render_template('main.html', title='home')
