35ee292fe414d65cf2c8b792066408973a6d39d3
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-17 21:11:19 +0330

    login

--- models.py
+++ models.py
@@ -1,17 +1,17 @@
-from run import db
+"""
+this is a model for todo
+"""
+from run import db, login_manager
+from flask_login import UserMixin
 
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(20), nullable=False)
-    done = db.Column(db.Boolean)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f'{self.id}, {self.task}-{self.done}'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
+    """ this is user model """
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
