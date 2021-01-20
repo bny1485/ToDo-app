4a4417f6d60191473596bd549c29e31db58f83e8
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-20 23:56:45 +0330

    delete todo

--- task/routes.py
+++ task/routes.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, abort
 from flask_login import login_required, current_user
 from task.forms import Todo
 from models import Task
@@ -20,3 +20,14 @@ def create_todo():
         db.session.commit()
         return redirect(url_for('task.create_todo'))
     return render_template('todo.html', title="todo", form=form, user_tasks=user_tasks, model_title="new task to do")
+
+
+@task.route('/task/<int:task_id>/delete', methods=['POST', 'GET'])
+@login_required
+def delete(task_id):
+    user_task = Task.query.get_or_404(task_id)
+    if user_task.author != current_user:
+        abort(403)
+    db.session.delete(user_task)
+    db.session.commit()
+    return redirect(url_for('task.create_todo'))
